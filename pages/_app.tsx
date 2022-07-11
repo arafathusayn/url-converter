9ca3b6249e725a80cd830c4e9e8000f4b8207a4e
Arafat Husayn <hello@arafat.dev>
2022-07-12 02:34:32 +0600

    feat: mvp

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,58 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import React, { useState } from "react";
+import type { AppProps } from "next/app";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import createCache from "@emotion/cache";
+import { CacheProvider } from "@emotion/react";
+import "../styles/globals.css";
+interface AppContext {
+  colorMode: "light" | "dark";
+  toggleColorMode: () => void;
 }
 
-export default MyApp
+const defaultContext = {
+  colorMode: "light",
+  toggleColorMode: () => {},
+} as const;
+
+export const ColorModeContext = React.createContext<AppContext>(defaultContext);
+
+const cache = createCache({ key: "next" });
+
+function MyApp({ Component, pageProps }: AppProps) {
+  const [colorMode, setColorMode] = useState<AppContext["colorMode"]>(
+    defaultContext.colorMode,
+  );
+
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: colorMode,
+        },
+      }),
+    [colorMode],
+  );
+
+  const toggleColorMode = () => {
+    setColorMode(colorMode === "light" ? "dark" : "light");
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <ColorModeContext.Provider
+        value={{
+          colorMode,
+          toggleColorMode,
+        }}
+      >
+        <CacheProvider value={cache}>
+          <Component {...pageProps} />
+        </CacheProvider>
+      </ColorModeContext.Provider>
+    </ThemeProvider>
+  );
+}
+
+export default MyApp;
